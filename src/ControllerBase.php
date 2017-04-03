b9f1b95b5054f9d95535502f575c7781c0c1b390
Roman Zimmermann <roman@more-onion.com>
2017-04-03 10:27:50 +0200

    Put some generic code in a controller base-class.

---
+++ src/ControllerBase.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\payone_payment;
+
+abstract class ControllerBase extends \PaymentMethodController {
+
+  public $payment_method_configuration_form_elements_callback = 'payment_forms_method_configuration_form';
+
+  public $controller_data_defaults = [
+    'mid' => '',
+    'portalid'  => '',
+    'aid' => '',
+    'api_key' => '',
+    'live' => 0,
+    'field_map' => [
+      'salutation' => ['salutation'],
+      'title' => ['title'],
+      'firstname' => ['first_name'],
+      'lastname' => ['last_name'],
+      'company' => ['company'],
+      'street' => ['street', 'street_address'],
+      'addressaddition' => [],
+      'zip' => ['zip_code', 'postcode'],
+      'city' => ['city'],
+      'state' => ['state'],
+      'country' => ['country'],
+      'email' => ['email'],
+      'telephonenumber' => ['phone_number'],
+      'birthday' => ['date_of_birth'],
+      'language' => ['language'],
+      'vatid' => ['vatid'],
+      'gender' => ['gender'],
+    ],
+  ];
+
+  public function configurationForm(\PaymentMethod $method) {
+    return new CreditCardConfigurationForm();
+  }
+
+  /**
+   * Generate a unique ID for this payment attempt.
+   */
+  public function generateReference(\Payment $payment) {
+    // If needed save the payment to get actual Ids.
+    if (!$payment->pid || !$payment->getStatus()->psiid) {
+      entity_save('payment', $payment);
+    }
+    $status = $payment->getStatus();
+    return $payment->pid . '-' . $status->psiid;
+  }
+
+}
+
--- src/CreditCardController.php
+++ src/CreditCardController.php
@@ -2,36 +2,8 @@
 
 namespace Drupal\payone_payment;
 
-class CreditCardController extends \PaymentMethodController {
+class CreditCardController extends ControllerBase {
   public $payment_configuration_form_elements_callback = 'payment_forms_payment_form';
-  public $payment_method_configuration_form_elements_callback = 'payment_forms_method_configuration_form';
-
-  public $controller_data_defaults = [
-    'mid' => '',
-    'portalid'  => '',
-    'aid' => '',
-    'api_key' => '',
-    'live' => 0,
-    'field_map' => [
-      'salutation' => ['salutation'],
-      'title' => ['title'],
-      'firstname' => ['first_name'],
-      'lastname' => ['last_name'],
-      'company' => ['company'],
-      'street' => ['street', 'street_address'],
-      'addressaddition' => [],
-      'zip' => ['zip_code', 'postcode'],
-      'city' => ['city'],
-      'state' => ['state'],
-      'country' => ['country'],
-      'email' => ['email'],
-      'telephonenumber' => ['phone_number'],
-      'birthday' => ['date_of_birth'],
-      'language' => ['language'],
-      'vatid' => ['vatid'],
-      'gender' => ['gender'],
-    ],
-  ];
 
   public function __construct() {
     $this->title = t('PayONE Credit Card');
@@ -41,23 +13,11 @@ class CreditCardController extends \PaymentMethodController {
     return new CreditCardForm();
   }
 
-  public function configurationForm(\PaymentMethod $method) {
-    return new CreditCardConfigurationForm();
-  }
-
   /**
    * {@inheritdoc}
    */
   function validate(\Payment $payment, \PaymentMethod $payment_method, $strict) {
     parent::validate($payment, $payment_method, $strict);
-  }
-
-  public function generateReference(\Payment $payment) {
-    if (!$payment->pid || !$payment->getStatus()->psiid) {
-      entity_save('payment', $payment);
-    }
-    $status = $payment->getStatus();
-    return $payment->pid . '-' . $status->psiid;
   }
 
   public function execute(\Payment $payment, $api = NULL) {
